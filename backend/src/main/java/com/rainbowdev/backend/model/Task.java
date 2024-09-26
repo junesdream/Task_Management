e095e87f470b839fb093195a9d320fe2e4e319e1
junesdream <norisori@gmail.com>
2024-09-26 22:21:47 +0200

    fixed Task model

--- backend/src/main/java/com/rainbowdev/backend/model/Task.java
+++ backend/src/main/java/com/rainbowdev/backend/model/Task.java
@@ -1,19 +1,21 @@
 package com.rainbowdev.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
 @Entity
 public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String description;
+    @Column(name = "due_date", nullable = true)
     private LocalDate dueDate;
+
     private boolean completed;
 
     // Getter und Setter
